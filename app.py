8e0ed9451ef920537d3b59b46706192fa05df510
bernick4-cyber <bernick4@gmail.com>
2026-07-25 17:07:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,15 @@ st.set_page_config(
 # FILE SETTINGS
 # ---------------------------------------------------------
 EXCEL_FILE = Path(__file__).parent / "NFL Project 2025.xlsx"
+st.write("Loaded file:", excel_file)
 
+st.write(
+    df[
+        df["Player"]
+        .astype(str)
+        .str.contains("Brian Thomas", case=False, na=False)
+    ][["Player"]]
+)
 
 # Full NFL team name to abbreviation
 TEAM_ABBREVIATIONS = {
